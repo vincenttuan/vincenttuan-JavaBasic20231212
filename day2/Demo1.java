1672ad0c4eb84aad90a22da7441d6fdfd102f5c1
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-14 19:09:01 +0800

    java

---
+++ day2/Demo1.java
@@ -0,0 +1,26 @@
+/**
+ * Java 變數的應用
+ * 基本變數 (8大)
+ * byte    8bis    -128 ~ +127
+ * short   16bits  -32768 ~ +32767
+ * int     32bits  -21億 ~ +21億
+ * long    64bits  -900京 ~ +900京
+ * float   32bits
+ * double  64 bits
+ * char    16bits  字元/0~65535的數字
+ * boolean 1或8 bits(根據 OS) true/false
+ * 
+ * 參考變數 (基本變數之外)
+ * String 字串
+ * Integer 
+ * Double 
+ * ...
+ */
+
+ package day2;
+
+ public class Demo1 {
+    public static void main(String[] args) {
+        
+    }
+ }
