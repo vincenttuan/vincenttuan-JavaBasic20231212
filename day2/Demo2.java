a78534d6666af9ebeefa0aad53b7642b6e2f1f49
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-14 19:46:40 +0800

    java

--- day2/Demo2.java
+++ day2/Demo2.java
@@ -8,7 +8,16 @@ public class Demo2 {
         int shares = 2; // 買進張數(1張 = 1000)
         double closePrice = 102; // 今日收盤價
         // 試問此股票賺賠多少錢 ?
-        
+        // 計算總投資額與股票現值
+        double totalInvestment = cost * shares * 1000; // 總投資額
+        double currentValue = closePrice * shares * 1000; // 股票現值
+        // 計算總利潤或虧損
+        double profit = currentValue - totalInvestment; // 股票現值 - 總投資額
+
+        System.out.printf("總投資額: %,.0f%n", totalInvestment);
+        System.out.printf("股票現值: %,.0f%n", currentValue);
+        System.out.printf("總利潤: %,.0f%n", profit);
+
 
     }
 }
