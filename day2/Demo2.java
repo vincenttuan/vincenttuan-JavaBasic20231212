f025a223fb9b6b5d0e6225975a8c708c446130fd
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-14 19:37:57 +0800

    java

---
+++ day2/Demo2.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        String stockName = "鴻海"; // 股票名稱
+        String stockNo = "2317.TW"; // 股票代號
+        double cost = 99.5; // 買進成本(每股)
+        int shares = 2; // 買進張數(1張 = 1000)
+        double closePrice = 102; // 今日收盤價
+        // 試問此股票賺賠多少錢 ?
+        
+
+    }
+}
