324fc982789cdfa8cc1a7381d76f5b6f781b3ecd
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-14 20:38:17 +0800

    java

--- day2/Demo3.java
+++ day2/Demo3.java
@@ -22,5 +22,33 @@ package day2;
  */
 
 public class Demo3 {
-    
+    public static void main(String[] args) {
+        // 股票
+        double stockAmount = 100_000;
+        double stockReturnRate = 0.08;
+        int stockRisk = 4;
+        // 債券
+        double bondAmount = 50_000;
+        double bondReturnRate = 0.03;
+        double bondRisk = 2;
+        // 房地產
+        double realEstateAmount = 150_000;
+        double realEstateReturnRate = 0.06;
+        double realEStateRisk = 3;
+
+        // 1. 每一項的投資預期年收益為何?
+        double stockReturn = stockAmount * stockReturnRate;
+        double bondReturn = bondAmount * bondReturnRate;
+        double realEstateReturn = realEstateAmount * realEstateReturnRate;
+
+        // 2. 總投資額和年收益總額各是多少?
+        double totalInvestment = stockAmount + bondAmount + realEstateAmount;
+        double totalReturn = stockReturn + bondReturn + realEstateReturn;
+
+        System.out.printf("股票預期年收益 : %,.0f%n", stockReturn);
+        System.out.printf("債券預期年收益 : %,.0f%n", bondReturn);
+        System.out.printf("房地產預期年收益 : %,.0f%n", realEstateReturn);
+        System.out.printf("總投資額 : %,.0f%n", totalInvestment);
+        System.out.printf("年收益總額 : %,.0f%n", totalReturn);
+    }
 }
