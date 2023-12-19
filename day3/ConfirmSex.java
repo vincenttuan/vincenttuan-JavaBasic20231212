f26f5a22f77ce3ed3b7745b590ff12f87c07892b
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-19 21:08:02 +0800

    java

--- day3/ConfirmSex.java
+++ day3/ConfirmSex.java
@@ -2,7 +2,7 @@ package day3;
 
 public class ConfirmSex {
     public static void main(String[] args) {
-        String id = "A123456789";
+        String id = "A163456789";
         char area = id.charAt(0); // 戶籍地
         char sex  = id.charAt(1);  // 性別
         char type = id.charAt(2); // 籍別
@@ -37,6 +37,31 @@ public class ConfirmSex {
            8     入籍國民，原為香港居民或澳門居民
            9     入籍國民，原為大陸地區居民
         */
-        
+        System.out.printf("type: %c%n", type);
+        switch (type) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+                System.out.println("在臺灣出生之本籍國民");
+                break;
+            case '6':
+                System.out.println("入籍國民，原為外國人");
+                break;
+            case '7':
+                System.out.println("入籍國民，原為無戶籍國民");
+                break;
+            case '8':
+                System.out.println("入籍國民，原為香港居民或澳門居民");
+                break;
+            case '9':
+                System.out.println("入籍國民，原為大陸地區居民");
+                break;
+            default:
+                System.out.println("籍別錯誤");
+                //break;
+        }
     }
 }
