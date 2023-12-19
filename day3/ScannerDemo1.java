8b27a00aedfffca99f56300434e5971365f615ef
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-19 18:55:35 +0800

    java

--- day3/ScannerDemo1.java
+++ day3/ScannerDemo1.java
@@ -12,5 +12,6 @@ public class ScannerDemo1 {
         // 計算 bmi
         double bmi = w / Math.pow(h/100, 2);
         System.out.printf("身高: %.1f 體重: %.1f BMI: %.2f%n", h, w, bmi);
+        
     }
 }
