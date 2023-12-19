b873bb5ca35447ef6eb749ef200a99b51d2175d0
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-19 19:12:00 +0800

    java

---
+++ day3/GradeEvaluator.java
@@ -0,0 +1,16 @@
+package day3;
+
+/**
+ * 學生成績評等
+ * 90 ~ 100 優秀 A
+ * 80 ~ 89  良好 B
+ * 70 ~ 79  中等 C
+ * 60 ~ 69  及格 D
+ * 0  ~ 59  不及格 F
+ * 其他分數: 成績錯誤 E
+*/
+public class GradeEvaluator {
+    public static void main(String[] args) {
+        
+    }
+}
