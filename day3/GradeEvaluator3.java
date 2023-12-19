143f38cf4f07c178d47cb9976fb78cbc147df565
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-19 21:16:40 +0800

    java

---
+++ day3/GradeEvaluator3.java
@@ -0,0 +1,22 @@
+package day3;
+
+import java.util.Scanner;
+
+/**
+ * 學生成績評等
+ * 90 ~ 100 優秀 A
+ * 80 ~ 89 良好 B
+ * 70 ~ 79 中等 C
+ * 60 ~ 69 及格 D
+ * 0 ~ 59 不及格 F
+ * 其他分數: 成績錯誤 E
+ */
+public class GradeEvaluator3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請輸入學生成績:");
+        int score = scanner.nextInt();
+        // 請用 switch-case 來判斷評等
+        
+    }
+}
