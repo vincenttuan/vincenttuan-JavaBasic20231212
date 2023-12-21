3e6ff3dae58d8d352b696c07667b58ff7867ef25
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-21 21:25:08 +0800

    java

--- day4/DoWhileDemo1.java
+++ day4/DoWhileDemo1.java
@@ -8,7 +8,6 @@ public class DoWhileDemo1 {
         Random random = new Random();
         int answer = random.nextInt(9) + 1; // 1~9 的隨機數
         Scanner scanner = null;
-        
         do {
             scanner = new Scanner(System.in);
             // 玩家開始
@@ -22,6 +21,19 @@ public class DoWhileDemo1 {
                 System.out.println("玩家猜小了");
             } else {
                 System.out.println("玩家猜對了, GG");
+                break;
+            }
+
+            // 模擬電腦參與猜數字
+            int pcGuess = random.nextInt(9) + 1; // 1~9 的隨機數
+            System.out.printf("電腦所猜的數字是:%d%n", pcGuess);
+            // 判斷電腦所猜的數字
+            if(pcGuess > answer) {
+                System.out.println("電腦猜大了");
+            } else if(pcGuess < answer) {
+                System.out.println("電腦猜小了");
+            } else {
+                System.out.println("電腦猜對了, GG");
                 break;
             }
             
