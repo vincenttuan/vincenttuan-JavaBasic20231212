2176529edc39feacae03354d5dc933a6ccc7a6d7
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-21 19:51:38 +0800

    java

--- day4/WhileDemo4.java
+++ day4/WhileDemo4.java
@@ -2,14 +2,15 @@ package day4;
 
 public class WhileDemo4 {
     public static void main(String[] args) {
-        int number = 13;
+        int number = 333;
         // number 是否是質數 ?
         int i = 2;
         boolean isPrime = true; // 是否為質數(預設: true)
-        while (i <= number/2) {
+        while (i <= Math.sqrt(number)) {
             System.out.printf("%d %% %d = %d%n", number, i, number%i);
             if(number%i == 0) {
                 isPrime = false;
+                break;
             }
             i++; //i = i + 1;
         }
