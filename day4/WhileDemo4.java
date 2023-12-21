511547e9b7145ca1174656f487763a4ad23cda3b
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-21 19:31:03 +0800

    java

--- day4/WhileDemo4.java
+++ day4/WhileDemo4.java
@@ -2,12 +2,17 @@ package day4;
 
 public class WhileDemo4 {
     public static void main(String[] args) {
-        int number = 9;
+        int number = 13;
         // number 是否是質數 ?
         int i = 2;
+        boolean isPrime = true; // 是否為質數(預設: true)
         while (i <= number/2) {
-            System.out.println(i);
+            System.out.printf("%d %% %d = %d%n", number, i, number%i);
+            if(number%i == 0) {
+                isPrime = false;
+            }
             i++; //i = i + 1;
         }
+        System.out.printf("%d %s%n", number, (isPrime ? "是質數" : "不是質數"));
     }
 }
