d69dc8622827b052bc0f77bb26ccbaec2fde2a8c
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-21 20:56:43 +0800

    java

--- day4/WhileDemo6.java
+++ day4/WhileDemo6.java
@@ -1,19 +1,22 @@
 package day4;
 
+import java.util.Scanner;
+
 public class WhileDemo6 {
     public static void main(String[] args) {
         // N * N 乘法表
         // 若使用者輸入 9 就是 9 * 9 乘法表
         // 若使用者輸入 13 就是 13 * 13 乘法表
-
-        int n = 9;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請輸入數字:");
+        int n = scanner.nextInt();
 
         int x = 1;
         while (x <= n) {
 
             int y = 1;
             while (y <= n) {
-                System.out.printf("%2d*%2d=%2d ", x, y, (x * y));
+                System.out.printf("%2d*%2d=%3d ", x, y, (x * y));
                 y++;
             }
             System.out.println();
@@ -21,5 +24,6 @@ public class WhileDemo6 {
             x++;
         }
 
+        scanner.close();
     }
 }
