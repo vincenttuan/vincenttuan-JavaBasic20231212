b4566a7f566920760a976b801b118bbee1ed234d
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-21 19:02:17 +0800

    java

--- day4/WhileDemo2.java
+++ day4/WhileDemo2.java
@@ -8,6 +8,10 @@ public class WhileDemo2 {
 
         while (true) {
             int number = random.nextInt(100); // 0~99 的隨機數
+            // 若 number < 50 不要印出
+            if(number < 50) {
+                continue; // 中斷本次迴圈, 立即執行下一個迴圈
+            }
             System.out.println(number);
             if(number == 77) {
                 break; // 離開迴圈
