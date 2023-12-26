cf35acaf77182bbcc8eaf15de153af9a766c5bd7
vincent tuan <vincentjava@yahoo.com.tw>
2023-12-26 19:06:10 +0800

    java

---
+++ day5/Change.java
@@ -0,0 +1,15 @@
+package day5;
+
+// 找零錢程式
+public class Change {
+    public static void main(String[] args) {
+        int price = 23;
+        int cash = 100;
+        int ret = cash - price;
+        System.out.printf("可樂 $%d%n", price);
+        System.out.printf("付款 $%d%n", cash);
+        System.out.printf("應找 $%d%n", ret);
+        
+
+    }
+}
